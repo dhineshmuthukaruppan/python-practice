7ea1b8fe1936bb450c8b0c3cdf1ae98c1d80bc24
viperdhinesh <viper.dhinesh@gmail.com>
2020-08-16 16:24:13 +0530

    python if else

---
+++ basics/intro.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+n = 10
+world = True
+
+for i in range(0, n): 
+    print("hello world")  
+    if world == True:
+        print("world is true")
+
+
+""" 
+
+    #include<iostream>
+
+    int main(){
+        bool world = true;  
+        for(int i=0; i<10; i++){
+            cout << "hello world" << endl;
+            if(world == true){
+                cout << "world is true" << endl;
+            }
+        }
+
+        return 0;
+    }
+
+"""
